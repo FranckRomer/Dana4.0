31f4471fc6fc1043569a5a1744ace30c73da17e5
FranckRomer <paco_metal95@hotmail.com>
2022-11-16 09:41:02 -0600

    Cambios al css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab266bd2d53b2603"; }
+/******/ 		__webpack_require__.h = function() { return "71f65916307fac76"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,6 @@ import Headers from '../component/Headers/Headers'
 
 
 export default function Home() {
-  // const [modal, setModal] = React.useState(false)
-  // const invertirModal = () => {
-  //   setModal(!modal)
-  // }
-
   return (
     <div className={styles.container}>
 
@@ -23,15 +18,7 @@ export default function Home() {
 
       {/********************** HEADER **********************/}
       <Headers />
-      {/* <header className={styles.Headers}>
-        <div className={styles.home}>
-          <Link href={"/"} className={styles.LinkHome}>Home</Link>
-        </div>
-
-        <div className={styles.menu} onClick={invertirModal}>
-          <img src="menu.png" />
-        </div>
-      </header> */}
+      
 
       {/********************** MAIN **********************/}
       <main className={styles.main}>
@@ -44,20 +31,7 @@ export default function Home() {
         </section>
       </main>
       
-      {/********************* MODAL *********************/}
-      {/* {modal ?
-        <nav className={styles.nav} >
-          <div className={styles.modalOscuro} onClick={() => setModal(!modal)}></div>
-          <ul >
-            <Link href={"./login"}                className={styles.Link}><li>Login</li></Link>
-            <Link href={"./login/register"}       className={styles.Link}><li>Register</li></Link>
-            <Link href={"./dashboard"}       className={styles.Link}><li>Profile</li></Link>
-            <Link href={"./Configuraciones"}       className={styles.Link}><li>Ajustes</li></Link>
-            <Link href={"./Configuraciones/Info"}       className={styles.Link}><li>Info</li></Link>            
-          </ul>
-        </nav>
-        : ""
-      } */}
+      
 
     </div>
   )
